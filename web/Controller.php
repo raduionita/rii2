194bb66b550a96f3263490f6eff20feb21b88ef2
Radu Ionita <ionita.radu.ionut@gmail.com>
2016-12-20 11:29:48 +0200

    Initial commit

---
+++ web/Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace common\web;
+
+class Controller extends \yii\web\Controller
+{
+    public function init()
+    {
+        // @todo What if there's no module!?
+        $this->layout = '@'.$this->module->id.'/views/layout';
+        parent::init();
+    }
+}
